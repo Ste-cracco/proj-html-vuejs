f246beaf2375a675bf0e47bb0925d9de97ca3d9a
Ste-cracco <s.cracco@yahoo.it>
2022-09-14 15:07:19 +0200

    Header Top completato

---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="header_top flex">
+        <div class="container">
+        <div class="row">
+            <menu class="flex">
+            <ul class="menu flex">
+                Everything about Lifestyle, Travel and Gadgets
+                <li>
+                <a href="#" v-for="(item, i) in menuHeaderTop" :key="i"> 
+                    {{ item }}
+                </a>
+                </li>
+            </ul>
+            <div class="icone_social flex">
+                <font-awesome-icon icon="fa-brands fa-facebook-f" />
+                <font-awesome-icon icon="fa-brands fa-twitter" />
+                <font-awesome-icon icon="fa-brands fa-linkedin" />
+            </div>
+            </menu>  <!-- Fine Menu -->
+        </div> <!-- Fine Row Menu -->
+        </div> <!-- Fine Container Menu -->
+    </div> <!-- Fine Header Top -->    
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeaderTop',
+    props: {
+      menuHeaderTop: Array,
+    }
+  }
+  
+  </script>
+  
+  <style scoped lang="scss">
+  
+    .header_top {
+      background-color: #0088CC;
+      height: 50px;
+      align-items: center;
+  
+      ul {
+        color: white;
+        font-size: 0.9rem;
+        gap: 30px;
+  
+        a {
+        color: white;
+        text-transform: uppercase;
+        padding: 0 10px;
+        }
+      } 
+    
+    .icone_social {
+      margin-left: auto;
+      color: white;
+      gap: 30px;
+    }
+      
+    }
+  
+  </style>
+  
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <HeaderTop :menuHeaderTop="menuHeaderTop"/>
+  </header>
+</template>
+
+
+<script>
+import HeaderTop from "./HeaderTop.vue";
+
+export default {
+    name: "MainHeader",
+    components: { HeaderTop },
+    data() {
+    return {
+        menuHeaderTop: [
+          'Sign In', 'About Us', 'Contact Us', 'Buy Now'
+        ],
+    }
+  } 
+}
+
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
